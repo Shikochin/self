7ef2cc21f486d18f7fe97910e152a245358a2351
CQBerry <chinq6624@gmail.com>
2022-08-16 15:31:01 +0800

    perf: :zap: compress image size to improve website performance

--- .vuepress/head.ts
+++ .vuepress/head.ts
@@ -4,7 +4,7 @@ const headConfigs: HeadConfig[] = [
     ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['link', { rel: 'icon', href: '/assets/imgs/icons/favicon-192x192.png' }],
-    ['link', { rel: 'apple-touch-icon', href: '/assets/imgs/icons/favicon-384x384.png' }]
+    ['link', { rel: 'apple-touch-icon', href: '/assets/imgs/icons/favicon-192x192.png' }]
 ]
 
 export default headConfigs;
