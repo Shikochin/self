0126009867f0417fce12fe94b188020ade123107
CQBerry <chinq6624@gmail.com>
2022-08-30 21:43:37 +0800

    feat: :sparkles: add ease in / out animation to hitokoto

--- blog/.vuepress/components/hitokoto.vue
+++ blog/.vuepress/components/hitokoto.vue
@@ -4,12 +4,20 @@ import { ref, onMounted, computed } from "vue";
 const hitokoto = ref('')
 const count = ref(0)
 const tip = computed(() => `这已经是你第 ${count.value} 次刷新一言了！`)
+const show = ref(false)
 
 async function fetchHitokoto() {
+  show.value = false
   hitokoto.value = await fetch("https://v1.hitokoto.cn/?c=a&c=d&c=i")
     .then(res => res.json())
-    .then(data => data.hitokoto)
-    .catch(e => e);
+    .then(data => {
+      show.value = true
+      return data.hitokoto
+    })
+    .catch(e => {
+      show.value = true
+      return '你点的太快了！'
+    });
   count.value++
 }
 
@@ -20,5 +28,27 @@ onMounted(() => {
 </script>
 
 <template>
-  <blockquote><span @click="fetchHitokoto" :title="tip">{{  hitokoto  }}</span></blockquote>
+  <blockquote>
+    <Transition name="hitokoto" mode="out-in">
+      <p @click="fetchHitokoto" :title="tip" v-if="show">{{  hitokoto  }}</p>
+      <p @click="fetchHitokoto" :title="tip" v-else>{{  hitokoto  }}</p>
+    </Transition>
+  </blockquote>
 </template>
+
+<style scoped>
+.hitokoto-enter-active,
+.hitokoto-leave-active {
+  transition: all 0.25s ease-out;
+}
+
+.hitokoto-enter-from {
+  opacity: 0;
+  transform: translateY(30px);
+}
+
+.hitokoto-leave-to {
+  opacity: 0;
+  transform: translateY(-30px);
+}
+</style>
