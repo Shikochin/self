6e9c3dd094e83dfb5c4defd79448ed91ea990d35
CQBerry <chinq6624@gmail.com>
2022-08-30 13:40:47 +0800

    style: :fire: use computed props to rewrite tip var in hitokoto.vue

--- blog/.vuepress/components/hitokoto.vue
+++ blog/.vuepress/components/hitokoto.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
-import { ref, onMounted } from "vue";
+import { ref, onMounted, computed } from "vue";
 
 const hitokoto = ref('')
 const count = ref(0)
-const tip = ref('')
+const tip = computed(() => `这已经是你第 ${count.value} 次刷新一言了！`)
 
 async function fetchHitokoto() {
   hitokoto.value = await fetch("https://v1.hitokoto.cn/?c=a&c=d&c=i")
     .then(res => res.json())
     .then(data => data.hitokoto)
     .catch(e => e);
-  tip.value = `这已经是你第 ${++count.value} 次刷新一言了！`
+  count.value++
 }
 
 onMounted(() => {
