49fe66558700bbcc97d9346b62011bd5bdf3e72a
CQBerry <chinq6624@gmail.com>
2022-10-05 22:00:27 +0800

    fix: optimized friendlink.vue

--- blog/.vuepress/components/friendlink.vue
+++ blog/.vuepress/components/friendlink.vue
@@ -1,55 +1,49 @@
 <script lang="ts" setup>
 import { onBeforeMount, ref } from "vue";
-
-const links = ref([]);
-onBeforeMount(async () => {
-  links.value = await fetch("/assets/links.json")
-      .then((res) => res.json())
-      .then((data) => data.links);
-});
+import friend from "../public/assets/friend.json";
 </script>
 
 <template>
-  <div id="linksGrid">
-    <div v-for="{ id, name, link, icon, desc } of links" :key="id" class="friendLink">
-      <img :alt="`${name}-${desc}`" :src="icon" class="icon"/>
-      <a :href="link">
-        <p class="name">{{ name }}</p>
-        <p>{{ desc }}</p>
-      </a>
+    <div id="linksGrid">
+        <div v-for="{ id, name, link, icon, desc } of friend.links" :key="id" class="friendLink">
+            <img :alt="`${name}-${desc}`" :src="icon" class="icon" />
+            <a :href="link">
+                <p class="name">{{ name }}</p>
+                <p>{{ desc }}</p>
+            </a>
+        </div>
     </div>
-  </div>
 </template>
 
 <style lang="scss" scoped>
 #linksGrid {
-  display: grid;
-  gap: 1vh 0.7vw;
-  grid-template-columns: repeat(auto-fit, minmax(280px, auto));
+    display: grid;
+    gap: 1vh 0.7vw;
+    grid-template-columns: repeat(auto-fit, minmax(280px, auto));
 
-  .friendLink {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    min-height: 150px;
-    height: var(--friendlink-height);
-    border-radius: var(--friendlink-border-radius);
-    border-color: var(--friendlink-border-color);
-    border-style: solid;
-    padding-left: 30px;
-    padding-right: 30px;
+    .friendLink {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        min-height: 150px;
+        height: var(--friendlink-height);
+        border-radius: var(--friendlink-border-radius);
+        border-color: var(--friendlink-border-color);
+        border-style: solid;
+        padding-left: 30px;
+        padding-right: 30px;
 
-    .icon {
-      width: 96px;
-      height: 96px;
-      border-radius: 100%;
-      margin-right: 0.8vw;
+        .icon {
+            width: 96px;
+            height: 96px;
+            border-radius: 100%;
+            margin-right: 0.8vw;
+        }
+
+        .name {
+            font-size: var(--friendlink-fontsize);
+            font-weight: bold;
+        }
     }
-
-    .name {
-      font-size: var(--friendlink-fontsize);
-      font-weight: bold;
-    }
-  }
 }
 </style>
