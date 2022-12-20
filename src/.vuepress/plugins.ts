44bde2e8534eb9e698b5977a642c91ac2d436993
CQBerry <cqbery@gmail.com>
2022-12-20 10:22:55 +0800

    chore(deps): remove @vuepress/plugin-search

--- src/.vuepress/plugins.ts
+++ src/.vuepress/plugins.ts
@@ -1,27 +1,23 @@
-import type { PluginConfig } from "vuepress";
-import { commentPlugin } from "vuepress-plugin-comment2";
-import { registerComponentsPlugin } from "@vuepress/plugin-register-components";
-import { path } from "@vuepress/utils";
-import { searchPlugin } from "@vuepress/plugin-search";
+import type { PluginConfig } from 'vuepress'
+import { commentPlugin } from 'vuepress-plugin-comment2'
+import { registerComponentsPlugin } from '@vuepress/plugin-register-components'
+import { path } from '@vuepress/utils'
 
 const pluginConfigs: PluginConfig = [
-  commentPlugin({
-    comment: true,
-    provider: "Giscus",
-    repo: "CQBerry/blog",
-    repoId: "R_kgDOHzsJjA",
-    category: "Comments",
-    categoryId: "DIC_kwDOHzsJjM4CSnTe",
-  }),
-  registerComponentsPlugin({
-    components: {
-      friendlink: path.resolve(__dirname, "./components/friendlink.vue"),
-      hitokoto: path.resolve(__dirname, "./components/hitokoto.vue"),
-    },
-  }),
-  searchPlugin({
-    hotKeys: ["ctrl", "k"],
-  }),
-];
+    commentPlugin({
+        comment: true,
+        provider: 'Giscus',
+        repo: 'CQBerry/blog',
+        repoId: 'R_kgDOHzsJjA',
+        category: 'Comments',
+        categoryId: 'DIC_kwDOHzsJjM4CSnTe',
+    }),
+    registerComponentsPlugin({
+        components: {
+            friendlink: path.resolve(__dirname, './components/friendlink.vue'),
+            hitokoto: path.resolve(__dirname, './components/hitokoto.vue'),
+        },
+    }),
+]
 
-export default pluginConfigs;
+export default pluginConfigs
